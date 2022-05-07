b97668565b1c97a871636679cde114f36bf1d820
samuelhuicabpastrana <samypas2001@hotmail.com>
2022-05-07 11:29:11 -0500

    no sé

--- AdminIBEM/admin-ajax.js
+++ AdminIBEM/admin-ajax.js
@@ -109,40 +109,5 @@ $(document).ready(function() {
   });
 
 
-  $('#listausuarios').on('submit', function(e) {
-
-    e.preventDefault();
-
-    var datos = $(this).serializeArray();
-
-    $.ajax({
-      type: $(this).attr('method'),
-      data: datos,
-      url: $(this).attr('action'),
-      dataType: 'json',
-      success: function(data) {
-        var resultado = data;
-        if (resultado.respuesta == 'correcto') {
-          Swal.fire(
-                    'Genial!',
-                    'Se ha actualizado con exito!',
-                    'success'
-                  )
-                  setTimeout(function(){
-                    window.location.href = 'listausuarios.php';
-                  }, 1000);
-        }else {
-          Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Algo ha salido mal, contacte a su administador del sistema',
-                    })
-        }
-      }
-
-
-
-    })
-  });
 
 });
